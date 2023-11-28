6647ec2b216e81f91491a9818c2fae1fb4224f48
Marko <mk0000v@gmail.com>
2023-11-28 17:51:19 +0100

    Update LV3

--- LV3/Zad1.py
+++ LV3/Zad1.py
@@ -1,7 +1,8 @@
 
 dictionary = {"identifikatori": {},
               "separatori": {},
-              "operatori": {}
+              "operatori": {},
+              "konstante": {}
               }
 text = input("Enter your line:\n")
 
@@ -11,13 +12,16 @@ for letter in text:
         print(f"('{letter}', identifikator)")
         dictionary['identifikatori'][letter]=dictionary.get('identifikatori',{}).get(letter,0)+1
     # Check if it is a operator
-    elif letter in ['=','+','*']:
+    elif letter in ['=','+','*','-','%']:
         print(f"('{letter}', operator)")
         dictionary['operatori'][letter]=dictionary.get('operatori',{}).get(letter,0)+1
     # Check if it is a separator
-    elif letter == ' ':
+    elif letter in [' ',';']:
         print(f"('{letter}', separator)")
         dictionary['separatori'][letter]=dictionary.get('separatori',{}).get(letter,0)+1
+    elif letter in ['1','2','3','4','5']:
+        print(f"('{letter}', konstante)")
+        dictionary['konstante'][letter]=dictionary.get('konstante',{}).get(letter,0)+1
 
 for outer_key,inner_dict in dictionary.items():
     print(f"{outer_key} [{sum(inner_dict.values())}]:", end=' ')
